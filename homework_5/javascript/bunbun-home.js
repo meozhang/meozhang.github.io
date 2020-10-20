5486aaf62ce9dac7832ce3441cfbefba6dd5782c
Meo Zhang <meozhang@gmail.com>
2020-10-19 20:31:37 -0400

    home page choose order number, add to cart function, update the cart number

--- homework_5/javascript/bunbun-home.js
+++ homework_5/javascript/bunbun-home.js
@@ -1,3 +1,7 @@
+//GLOBAL VARIBLES
+var totalCartNum = 0;
+
+//UPDATE THE PRODUCT DETAIL MODAL
 // Get the modal
 var modal = document.getElementById("modal");
 
@@ -9,15 +13,14 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the products, open the modal 
 for (i = 0; i < products.length; i++) { 
-    const currentProductIndex = i;
-    products[currentProductIndex].getElementsByClassName("product-picture")[0].onclick = function() {
+    products[i].getElementsByClassName("product-picture")[0].onclick = function() {
         modal.style.display = "block";
 
         // take product values
-        const productDescription = products[currentProductIndex].getElementsByClassName("product-description")[0].innerText;
-        const productPrice = products[currentProductIndex].getElementsByClassName("product-price")[0].innerText;
-        const productTitle = products[currentProductIndex].getElementsByClassName("product-title")[0].innerText;
-        const productPicture = products[currentProductIndex].getElementsByClassName("product-picture")[0];
+        const productDescription = this.parentElement.getElementsByClassName("product-description")[0].innerText;
+        const productPrice = this.parentElement.getElementsByClassName("product-price")[0].innerText;
+        const productTitle = this.parentElement.getElementsByClassName("product-title")[0].innerText;
+        const productPicture = this.parentElement.getElementsByClassName("product-picture")[0];
 
         // assign to modal values
         modal.getElementsByClassName("modal-description")[0].innerText = productDescription;
@@ -37,4 +40,58 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+//SELECT THE NUMBER + ADD TO CART
+
+// create a list of all products
+var allProduct= document.getElementsByClassName("product-block");
+
+// Loop all the product, 
+for (i = 0; i < allProduct.length; i++) {
+    //get the number choices in a list
+    var orderNumChoice = allProduct[i].getElementsByClassName("order-num");
+    console.log(orderNumChoice);
+
+    for(j = 0; j < orderNumChoice.length; j++){
+        console.log(orderNumChoice[j]);
+        orderNumChoice[j].onclick = function(){
+
+            //make all other numbers inactive
+            var current = this.parentElement.getElementsByClassName("active");
+            if (current.length > 0) { 
+                current[0].className = current[0].className.replace(" active", "");
+            }    
+            //make the clicked number active
+            this.className += " active";
+        }
+    }
+}
+
+// Add To Cart
+for (i = 0; i < allProduct.length; i++) {
+    var addBtn = allProduct[i].getElementsByTagName("button");
+    addBtn[0].onclick = function(){
+
+        //find the current active number choice
+        var orderNumActive = this.parentElement.getElementsByClassName("active");
+
+        //update totalCartNum
+        totalCartNum += parseInt(orderNumActive[0].innerHTML);
+        console.log(totalCartNum);
+
+        //assign new number to display in HTML
+        document.getElementById("cart-num").innerHTML = totalCartNum;
+        
+        setTimeout(displayAdded(this), 1500);
+        
+        //remove the active choice
+        orderNumActive[0].className = orderNumActive[0].className.replace(" active", "");
+
+    }
+}
+
+//button display "added" onclick
+function displayAdded(button){
+    button.innerHTML="added &#10003;";
 }
